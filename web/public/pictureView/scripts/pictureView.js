42f398316e80e9fa0809c7d108d659774724b583
chgj1995 <chgj1995@naver.com>
2025-06-24 18:36:52 +0900

    try to resolve lag problem
    try2

--- web/public/pictureView/scripts/pictureView.js
+++ web/public/pictureView/scripts/pictureView.js
@@ -34,8 +34,29 @@ function loadPictures(pictures) {
 
         if (window.pictureViewer) {
             window.pictureViewer.destroy();
+            window.pictureViewer = null;
         }
-        window.pictureViewer = new Viewer(gridContainer);
+
+        window.pictureViewer = new Viewer(gridContainer,{
+            inline: false,
+            button: true,
+            navbar: false,
+            title: true,
+            movable: false,
+            toolbar: {
+                zoomIn: 1,         // 확대
+                zoomOut: 1,        // 축소
+                oneToOne: 0,       // 원본 크기
+                reset: 1,          // 초기 상태로 복원
+                prev: 1,           // 이전 이미지
+                play: { show: 0 }, // 슬라이드쇼 (0 = 숨김)
+                next: 1,           // 다음 이미지
+                rotateLeft: 1,     // 왼쪽으로 회전
+                rotateRight: 1,    // 오른쪽으로 회전
+                flipHorizontal: 0, // 수평 뒤집기
+                flipVertical: 0    // 수직 뒤집기
+            }
+        });
     });
 }
 
@@ -44,8 +65,6 @@ document.addEventListener('dataUpdated', (event) => {
     console.log('dataUpdated:', event.detail);
     loadPictures(event.detail);
 });
-
-
 
 // 로딩 인디케이터 표시 제어
 const loadingIndicator = document.getElementById('loading-indicator');
